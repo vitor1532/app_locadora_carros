4db5eaef06e015331e07bbc5bddab56a363ba07f
Aristopato <vitomartinsalmeida@gmail.com>
2022-11-16 15:51:35 -0300

    abstraindo repositórios para AbstractRepository

--- app/Repositories/MarcaRepository.php
+++ app/Repositories/MarcaRepository.php
@@ -4,36 +4,8 @@
 
     use Illuminate\Database\Eloquent\Model;
 
-    class MarcaRepository {
+    class MarcaRepository extends AbstractRepository {
 
-        public function __construct(Model $model) {
-            $this->model = $model;
-        }
-        public function selectAtributosRegistrosRelacionados($atributos) {
-            $this->model = $this->model->with($atributos);
-            //$marcas = $this->marca->with();
-        }
-
-        public function filtro($filtros) {
-            $filtros = explode(';', $filtros);
-            //dd($filtros);
-
-            foreach($filtros as $key => $conditional) {
-
-                $c = explode(':', $conditional);
-
-                $this->model = $this->model->where($c[0], $c[1], $c[2]);
-            }
-        }
-
-        public function selectAtributos($attr) {
-            $this->model = $this->model->selectRaw($attr);
-        }
-
-        public function getResult() {
-            //dd($this->model);
-            return $this->model->get();
-        }
 
     }
 
--- app/Repositories/ModeloRepository.php
+++ app/Repositories/ModeloRepository.php
@@ -1,48 +1,13 @@
 <?php
 
 
-namespace App\Repositories;
+    namespace App\Repositories;
 
-use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Model;
 
-class ModeloRepository
-{
+    class ModeloRepository extends AbstractRepository {
 
-    public function __construct(Model $model)
-    {
-        $this->model = $model;
+
     }
-
-    public function selectAtributosRegistrosRelacionados($atributos)
-    {
-        $this->model = $this->model->with($atributos);
-        //$marcas = $this->marca->with();
-    }
-
-    public function filtro($filtros)
-    {
-        $filtros = explode(';', $filtros);
-        //dd($filtros);
-
-        foreach ($filtros as $key => $conditional) {
-
-            $c = explode(':', $conditional);
-
-            $this->model = $this->model->where($c[0], $c[1], $c[2]);
-        }
-    }
-
-    public function selectAtributos($attr)
-    {
-        $this->model = $this->model->selectRaw($attr);
-    }
-
-    public function getResult()
-    {
-        //dd($this->model);
-        return $this->model->get();
-    }
-
-}
 
 
