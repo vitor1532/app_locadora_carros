65aec632e5340d275c748724c96362fc22c86bde
Aristopato <vitomartinsalmeida@gmail.com>
2022-11-18 14:37:01 -0300

    resolvendo a parte de clientes

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,62 +3,128 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
+use App\Repositories\ClienteRepository;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
+
+    public function __construct(Cliente $cliente) {
+        $this->cliente = $cliente;
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $clienteRepository = new ClienteRepository($this->cliente);
+
+        $clientes = [];
+        //-----------------------
+
+        if($request->has('filtro')) {
+            $clienteRepository->filtro($request->filtro);
+        }
+
+        if($request->has('attr')) {
+
+            $attr = $request->attr;
+            $clienteRepository->selectAtributos($attr);
+
+        }
+
+        return response()->json($clienteRepository->getResult(), 200);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, Cliente $cliente)
     {
-        //
+        $request->validate($this->cliente->rules());
+
+        $modelo = $this->cliente->create([
+            'nome' => $request->nome
+        ]);
+
+        return response()->json($modelo, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Cliente  $cliente
-     * @return \Illuminate\Http\Response
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(Cliente $cliente)
+    public function show($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if($cliente === null) {
+            return response()->json(['erro' => 'registro não encontrado'], 404);
+        }
+
+        return response()->json($cliente, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Cliente  $cliente
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, Cliente $cliente)
+    public function update(Request $request, $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+
+        if($cliente === null) {
+            return response()->json(['erro' => 'registro não encontrado'], 404);
+        }
+
+        if($request->method() === 'PATCH') {
+
+            $regrasDinamicas = array();
+
+            foreach($cliente->rules() as $input => $regra) {
+
+                if(array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas);
+
+        } else {
+            $request->validate($cliente->rules());
+        }
+
+        $cliente->fill($request->all());
+        $cliente->save();
+
+        return response()->json($cliente, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Cliente  $cliente
-     * @return \Illuminate\Http\Response
+     * @param  Integer
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(Cliente $cliente)
+    public function destroy($id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if($cliente === null) {
+            return response()->json(['erro' => 'registro não encontrado'], 404);
+        }
+
+        $cliente->delete();
+        $msg = ['msg' => 'O cliente '."'".$cliente->nome."'".' foi deletada(o) com sucesso'];
+        return response()->json($msg, 200);
     }
 }
---
+++ app/Repositories/ClienteRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+class  ClienteRepository extends AbstractRepository
+{
+
+
+}
